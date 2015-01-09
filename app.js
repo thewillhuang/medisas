36ab711b2db33863c586e7f64945ff78c7b1ae2a
william huang <will.h86@gmail.com>
2015-01-08 21:35:53 -0800

    remodified to make it correct

--- app.js
+++ app.js
@@ -1,38 +1,62 @@
+var moment = require('moment');
+var _ = require('lodash');
 var Twitter = require('node-tweet-stream');
 var t = new Twitter({
-    consumer_key: '8AqQCy7umStCyNN356v7fw',
-    consumer_secret: 'vOvKV1QwuS1AeKPMIvJqErBxW7i1N12OL4UY2tNMs0c',
-    token: '29463499-9Og6hxW4HqFxcQyIrAdmLpbAnrwIk290ghOE0ez5f',
-    token_secret: 'elXVYJRFmFFit3PiVTmI9eU0IvHqqD7H4yeEmClJ8c'
-  });
-var moment = require('moment');
-var rollingWindowInMin = 1;
+  consumer_key: '8AqQCy7umStCyNN356v7fw',
+  consumer_secret: 'vOvKV1QwuS1AeKPMIvJqErBxW7i1N12OL4UY2tNMs0c',
+  token: '29463499-9Og6hxW4HqFxcQyIrAdmLpbAnrwIk290ghOE0ez5f',
+  token_secret: 'elXVYJRFmFFit3PiVTmI9eU0IvHqqD7H4yeEmClJ8c'
+});
 var tempObj = {};
+var topten = [];
+var top = 0;
+
+//user defined variables
+var rollingWindowInMin = 1;
 var refreshRateInMs = 1000;
+var rollingWindowInMs = rollingWindowInMin * 1000 * 6;
 
 t.on('tweet', function (tweet) {
   if (tweet.retweeted_status !== undefined) {
     var tweetText = tweet.retweeted_status.text;
     var retweetCount = tweet.retweeted_status.retweet_count;
-    tempObj[retweetCount] = tweetText;
+    tempObj[Date.now()] = {
+      text: tweetText,
+      count: retweetCount
+    };
   }
 });
 
 t.on('error', function (err) {
-  console.log('Oh no');
+  console.log('something is wrong with the stream');
 });
 
 t.track('all');
 
-
-var logTopTen = function() {
+var logTopTenText = function() {
   var keyArray = Object.keys(tempObj);
-  for (var i = keyArray.length - 1; i >= keyArray.length - 11; i--) {
-    console.log('text', tempObj[keyArray[i]]);
-    console.log('retweets', keyArray[i]);
+  var windowArray = _.filter(keyArray, function(num) {
+    if (num >= Date.now() - rollingWindowInMs) {
+      return num;
+    }
+  });
+  for (var i = 0; i < windowArray.length; i++) {
+    if (tempObj[windowArray[i]].count > top) {
+      topten.push({
+        text: tempObj[windowArray[i]].text,
+        count: tempObj[windowArray[i]].count
+      });
+    }
+    top = topten[topten.length - 1].count;
+    if (topten.length >= 10) {
+      topten.shift();
+    }
   }
+  console.log('key Array Length', keyArray.length);
+  console.log('window Array Length', windowArray.length);
+  console.log('top number', top);
+  console.log(topten);
   console.log('===========================================', moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 };
 
-var logEverySec = setInterval(logTopTen, refreshRateInMs);
-var reset = setTimeout(function(){tempObj = {};}, rollingWindowInMin*1000*60);
+var logEverySec = setInterval(logTopTenText, refreshRateInMs);

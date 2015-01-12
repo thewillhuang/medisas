262e64097e399e25f4d945b3a45592bf870cfc18
william huang <will.h86@gmail.com>
2015-01-12 15:36:14 -0800

    changed readme

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   config: {
-    consumer_key: 'your app key',
-    consumer_secret: 'your app key',
-    token: 'your app key',
-    token_secret: 'your app key'
+    consumer_key: '8AqQCy7umStCyNN356v7fw',
+    consumer_secret: 'vOvKV1QwuS1AeKPMIvJqErBxW7i1N12OL4UY2tNMs0c',
+    token: '29463499-9Og6hxW4HqFxcQyIrAdmLpbAnrwIk290ghOE0ez5f',
+    token_secret: 'elXVYJRFmFFit3PiVTmI9eU0IvHqqD7H4yeEmClJ8c'
   }
 };
